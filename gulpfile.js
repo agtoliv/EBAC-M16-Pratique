f101dcb52ed77a29646fd5c177da1a17b64380dd
agtoliv <augustooliveira.silveira@gmail.com>
2024-09-21 10:29:17 -0300

    chore: add all imports to gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
